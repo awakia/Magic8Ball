3f046c5a31883ca58af1185c134f4d92ac96ff93
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-04-30 12:58:13 +0900

    Add label element

--- app/magic_8ball_view_controller.rb
+++ app/magic_8ball_view_controller.rb
@@ -5,5 +5,22 @@ class Magic8BallViewController < UIViewController
 
   def viewDidLoad
     view.image = UIImage.imageNamed('background.png')
+
+    @label = makeLabel
+    view.addSubview(@label)
   end
+
+  def makeLabel
+    # labelFrame = CGRectMake(10, 60, 300, 80)
+    labelFrame = [[10, 60], [300,80]]
+    label = UILabel.alloc.initWithFrame(labelFrame)
+
+    label.text = 'Tap for Answer!'
+    label.font = UIFont.boldSystemFontOfSize(34)
+    label.backgroundColor = UIColor.lightGrayColor
+    label.textColor = UIColor.darkGrayColor
+    label.textAlignment = UITextAlignmentCenter
+    label
+  end
+
 end
