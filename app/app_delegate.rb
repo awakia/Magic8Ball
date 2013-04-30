33bd5ab984b17f1dad32b4aa63afd03d576caf7d
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-04-30 12:34:38 +0900

    Create sample flashlight app

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,8 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    alert = UIAlertView.new
-    alert.message = "Hello World!"
-    alert.show
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.backgroundColor = UIColor.yellowColor
+    @window.makeKeyAndVisible
+    # main)> app = UIApplication.sharedApplication
+    # main)> repl(app.delegate)
+    # (#<AppDelegate:0xd2773b0 @windo...)> @window.backgroundColor = UIColor.greenColor
     true
   end
 end
