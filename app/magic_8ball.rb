4d8994dab2cdf674dddf8c3f5edb4e48ba610ec4
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-04-30 15:14:09 +0900

    Load data from json resources

--- app/magic_8ball.rb
+++ app/magic_8ball.rb
@@ -1,6 +1,35 @@
 class Magic8Ball
   def initialize
-    @answers = ['Yes', 'No', 'Maybe', 'Try Again']
+    @answers = loadAnswers
+  end
+
+  def loadAnswers
+    answerFile = NSBundle.mainBundle.pathForResource('answers', ofType:'json')
+    errorPointer = Pointer.new(:object)
+
+    data = NSData.alloc.initWithContentsOfFile(answerFile,
+      options:NSDataReadingUncached,
+      error:errorPointer)
+
+    unless data
+      printError error = errorPointer[0]
+      return ['X', 'Y']
+    end
+
+    json = NSJSONSerialization.JSONObjectWithData(data,
+      options:NSDataReadingUncached,
+      error:errorPointer)
+
+    unless json
+      printError error = errorPointer[0]
+      return ['X', 'Y']
+    end
+
+    json['answers']
+  end
+
+  def printError(error)
+    $stderr.puts "Error: #{error.description}"
   end
 
   def randomAnswer
